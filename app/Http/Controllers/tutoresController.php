2b6c90c2dce7d0049e3c3f8e4a76f8dae6045ab2
Jose Alfredo Valladares <josevalladaresvillanueva@gmail.com>
2024-07-08 13:12:46 -0600

    Muestra los datos de la vista de Inicio Para que Mike trabaje

--- app/Http/Controllers/tutoresController.php
+++ app/Http/Controllers/tutoresController.php
@@ -15,22 +15,6 @@ class tutoresController extends Controller
 
     }
 
-    public function mostrarContactos($id, Request $request){
-
-        $user = \Auth::user()->id;
-        $bibliotecas = Biblioteca::get()->where('tutor_id',$user);
-        $dependencia = Dependencia::get()->where('id_biblioteca',$id);
-
-        if ($request->ajax()) {
-            // Retornar una respuesta JSON
-            return response()->json([
-                'success' => true,
-                'dependencia' => $dependencia
-            ]);
-        }
-        return view('Tutor.index',compact('bibliotecas','dependencia'));
-
-    }
 
 
 
